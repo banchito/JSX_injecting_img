c0454107a3d508947bb2f0e5a1097f009e45eca8
Esteban Ordonez <44548586+banchito@users.noreply.github.com>
2020-10-22 08:38:35 -0700

    Inline Styling with JSON Look at <h6>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const customStyle = {
+  color: "blue",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
 var img = "https://picsum.photos/200";
 
 ReactDOM.render(
   <div>
     <h1 className="heading">My Favourite Foods</h1>
+    <h6 style={customStyle}> inline styling</h6>
     <div>
       <img src={img + "?grayscale"} alt="random" />
     </div>
