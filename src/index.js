a44ee815aabbc9105e8f380d1bf0aec69c8cedff
Esteban Ordonez <44548586+banchito@users.noreply.github.com>
2020-10-22 07:44:53 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+var img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <div>
+      <img src={img} />
+    </div>
+  </div>,
+  document.getElementById("root")
+);
